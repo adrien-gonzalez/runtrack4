22a9254ee106dc2ba80b4fb1a4df3fece7af83b7
adrien <adrien.gonzalez@laplateforme.io>
2020-05-04 10:23:11 +0200

    Job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Job03</title>
+	<link href="style.css" rel="stylesheet">
+</style>
+</head>
+
+<body>
+
+<main>
+	<div>
+		<section class="section">
+		</section>
+
+		<section class="section">
+		</section>
+	</div>
+	<div>
+		<section class="section">
+		</section>
+
+		<section class="section">
+		</section>
+	</div>
+</main>
+</body>
+
+</html>
