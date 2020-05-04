7df1fe23c5956caaf620da6c87580df3f940fcf3
adrien <adrien.gonzalez@laplateforme.io>
2020-05-04 09:38:55 +0200

    Job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Job01</title>
+	<link href="style1.css" rel="stylesheet">
+	<link href="style2.css" media="(min-width: 1680px) and (max-width: 1920px)" rel="stylesheet">
+	<link href="style3.css" media="(min-width: 1280px) and (max-width: 1680px)" rel="stylesheet">
+	<link href="style4.css" media="(max-width: 1280px)" rel="stylesheet">
+
+</style>
+</head>
+
+<body>
+
+
+
+
+</body>
+
+</html>
