538aad4007090b32ed789b9a3948dd1edf64037a
adrien <adrien.gonzalez@laplateforme.io>
2020-05-04 10:25:43 +0200

    suppr ligne inutile

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -7,7 +7,6 @@
 	<link href="style2.css" media="(min-width: 1680px) and (max-width: 1920px)" rel="stylesheet">
 	<link href="style3.css" media="(min-width: 1280px) and (max-width: 1680px)" rel="stylesheet">
 	<link href="style4.css" media="(max-width: 1280px)" rel="stylesheet">
-</style>
 </head>
 
 <body>
