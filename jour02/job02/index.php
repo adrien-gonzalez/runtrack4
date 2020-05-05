b2be246ebbefc0b170d46eb992db918065adb854
adrien <adrien.gonzalez@laplateforme.io>
2020-05-05 10:14:02 +0200

    Correction header

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -15,9 +15,10 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo right">Logo</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Recherche</a></li>
       </ul>
     </div>
   </nav>
