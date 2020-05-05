a541a71b493fd1ee853b360738cb398797cefc8a
adrien <adrien.gonzalez@laplateforme.io>
2020-05-05 11:14:14 +0200

    Job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Job05</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.7">
+		<!-- Compiled and minified CSS -->
+   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- ICONS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	</head>
+
+<body>
+
+<hearder>
+  <nav>
+    <div class="nav-wrapper grey lighten-1">
+      <a href="#" class="brand-logo"><img src="logo.png" width="50px">Volkswagen</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Accueil</a></li>
+        <li><a href="">lien 2</a></li>
+        <li><a href="">Lien 3</a></li>
+      </ul>
+    </div>
+  </nav>
+</hearder><br><br>
+
+  <div class="row">
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+          <img src="suv1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="suv2.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="suv3.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="suv4.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<div class="row">
+    <div class="col s12 m6 ">
+      <div class="card">
+        <div class="card-image">
+          <img src="v1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 ">
+      <div class="card">
+        <div class="card-image">
+          <img src="v2.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="https://www.volkswagen.fr/fr.html">This is a link</a>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+</html>
